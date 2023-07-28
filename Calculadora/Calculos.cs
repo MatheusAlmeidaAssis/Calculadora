5a128275d45399870cb9a6eac09d2cafeb3b53a2
MatheusAlmeidaAssis <m.almeidadeassis@gmail.com>
2023-07-28 12:08:04 -0300

    Adicionar arquivos de projeto.

---
+++ Calculadora/Calculos.cs
@@ -0,0 +1,37 @@
+﻿namespace Calculadora
+{
+    public class Calculos
+    {
+        public int Fatorial(int n)
+        {
+            if (n <= 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return Fatorial(n - 1) * n;
+            }
+        }
+
+        public int Soma(int n1, int n2)
+        {
+            return n1 + n2; 
+        }
+
+        public int Subtracao(int n1, int n2)
+        {
+            return n1 - n2;
+        }
+
+        public int Divisao(int n1, int n2)
+        {
+            return n1 / n2;
+        }
+
+        public int Multiplicacao(int n1, int n2)
+        {
+            return n1 * n2;
+        }        
+    }
+}
